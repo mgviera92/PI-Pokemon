eec8f138d2a8471f9aef4d3f5b733fc911b46d82
fireflies <gabiviera92@gmail.com>
2023-09-09 17:03:05 -0300

    changes

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(4532, () => {
     // console.log(`Server raised in port: ${PORT}`);// eslint-disable-line no-console
     console.log(`Server raised successfully`);
   });
