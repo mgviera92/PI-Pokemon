3aa9565ce98ae5082e6168959e30e15befb7ebc6
fireflies <gabiviera92@gmail.com>
2023-09-09 18:07:30 -0300

    deploy2

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const PORT = 5432;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
