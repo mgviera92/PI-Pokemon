b7b591c7483cbba56f9284edee9227d34f99840b
fireflies <gabiviera92@gmail.com>
2023-09-09 17:34:16 -0300

    ready

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -12,7 +12,7 @@ import {
   CREATE_POKEMON,
 } from "./action-types";
 
-const BACKEND_URL = "pokemon-app-server-5m9ac4zxs-mgviera92.vercel.app";
+const BACKEND_URL = "https://pokemon-app-server-exfx.onrender.com";
 
 export const getAllPokemons = () => {
   return async (dispatch) => {
