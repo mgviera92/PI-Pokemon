d3c18c8b35f6304f727099305ba0138309b5b55a
fireflies <gabiviera92@gmail.com>
2023-09-11 10:21:26 -0300

    deploy

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -12,7 +12,7 @@ import {
   CREATE_POKEMON,
 } from "./action-types";
 
-const BACKEND_URL = "https://pokemon-app-mgviera92.vercel.app";
+const BACKEND_URL = "https://pokemon-app-orpin-five.vercel.app";
 
 export const getAllPokemons = () => {
   return async (dispatch) => {
